555b7c8faecb6c44fe04bd8f45c5278c89269050
tlatkdgus1 <ipod1597@naver.com>
2016-12-28 02:12:50 +0900

    Smartalter

---
+++ include/llvm/Transforms/Smartalter/First.h
@@ -0,0 +1,34 @@
+#ifndef _FIRST_H_
+#define _FIRST_H_
+
+
+// LLVM include
+#include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Type.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/CodeGen/ISDOpcodes.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Transforms/IPO.h"
+#include <list>
+
+using namespace std;
+using namespace llvm;
+
+// Namespace
+namespace llvm {
+	Pass *createFirst ();
+	Pass *createFirst (bool flag);
+}
+#endif
---
+++ lib/Transforms/Smartalter/First.cpp
@@ -0,0 +1,48 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/Type.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/GlobalValue.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/CodeGen/ISDOpcodes.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Transforms/IPO.h"
+#include <list>
+
+#include "llvm/Transforms/Smartalter/First.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Linker/Linker.h"
+#include "llvm/Support/SourceMgr.h"
+using namespace llvm;
+namespace {
+	struct First : public FunctionPass {
+		static char ID; // Pass identification
+		bool flag;
+		First() : FunctionPass(ID) {}
+		First(bool flag) : FunctionPass(ID) {this->flag = flag; First();}
+
+		virtual bool runOnFunction(Function &F){
+			errs() << "Current function name: [" << F.getName() << "]\n";
+		}
+	};
+}
+
+char First::ID = 0;
+static RegisterPass<First> X("First", "llvm obfuscation first");
+
+Pass *llvm::createFirst() {
+	return new First();
+}
+
+Pass *llvm::createFirst(bool flag) {
+	return new First(flag);
+}
